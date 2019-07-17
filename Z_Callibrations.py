9a9da0c2d1adcd09bf68ef974ac51c02cc8062e0
Josh Lemberg <JoshLemberg@Joshuas-MacBook-Pro.local>
2019-07-17 01:12:35 -0400

    Adjustment for only Star-Forming Galaxies in correct temperature range

--- Z_Callibrations.py
+++ Z_Callibrations.py
@@ -39,11 +39,16 @@ def plot():
 
 ## Adjustment of the data for Err
 def adjustForErr():
+    # Opening tables
     table = astropy_data_read("dwarfdata.txt")
     fluxTable = astropy_data_read("dwarf_flux.txt")
     
-    table = astropy_data_read("dwarfdata.txt")
-    fluxTable = astropy_data_read("dwarf_flux.txt")
+    # Establishing R_23
+# =============================================================================
+#     R_23 = (fluxTable["OII_3727_FLUX"] + fluxTable["OIII_4959_FLUX"] + fluxTable["OIII_5007_FLUX"]) / fluxTable["H_BETA_FLUX"]
+# =============================================================================
+    
+    # Iteration to correct tables so they have the same galaxies
     i = 0
     while i < 9156:
         if (table["index"][i] != fluxTable["index"][i]):
@@ -58,10 +63,48 @@ def adjustForErr():
     goodIndexes = np.logical_and(BPTClassEqualsOneIndex, t3LessThan3Index)
     
 
-    plt.figure(1)
+# =============================================================================
+#     plt.figure(1)
+#     plt.xlabel("Z12logOH")
+#     plt.ylabel("R_23")
+#     plt.autoscale(enable = True, axis = 'both', tight = None) # Creates a semilog plot to effectively show data
+#     plt.semilogy(table["Z12logOH"][goodIndexes], R_23[goodIndexes], ".")
+#     plt.show()
+# =============================================================================
+    
+    plt.figure(2)
     plt.xlabel("Z12logOH")
+    plt.ylabel("[NII]\u03BB6584 / H_\u03B1")
     plt.autoscale(enable = True, axis = 'both', tight = None) # Creates a semilog plot to effectively show data
     plt.semilogy(table["Z12logOH"][goodIndexes], fluxTable["NII_6584_FLUX"][goodIndexes] / fluxTable["H_ALPHA_FLUX"][goodIndexes], ".")
+    plt.show()
+    
+    plt.figure(3)
+    plt.xlabel("Z12logOH")
+    plt.ylabel("[OIII]\u03BB5007 / [NII]\u03BB6584")
+    plt.autoscale(enable = True, axis = 'both', tight = None) # Creates a semilog plot to effectively show data
+    plt.semilogy(table["Z12logOH"][goodIndexes], fluxTable["OIII_5007_FLUX"][goodIndexes] / fluxTable["NII_6584_FLUX"][goodIndexes], ".")
+    plt.show()
+    
+    plt.figure(4)
+    plt.xlabel("Z12logOH")
+    plt.ylabel("[NII]\u03BB6584 / [OII]\u03BB3727")
+    plt.autoscale(enable = True, axis = 'both', tight = None) # Creates a semilog plot to effectively show data
+    plt.semilogy(table["Z12logOH"][goodIndexes], fluxTable["NII_6584_FLUX"][goodIndexes] / fluxTable["OII_3727_FLUX"][goodIndexes], ".")
+    plt.show()
+    
+    plt.figure(5)
+    plt.xlabel("Z12logOH")
+    plt.ylabel("[NII]\u03BB6584 / [SII]\u03BB6717")
+    plt.autoscale(enable = True, axis = 'both', tight = None) # Creates a semilog plot to effectively show data
+    plt.semilogy(table["Z12logOH"][goodIndexes], fluxTable["NII_6584_FLUX"][goodIndexes] / fluxTable["SII_6717_FLUX"][goodIndexes], ".")
+    plt.show()
+    
+    plt.figure(6)
+    plt.xlabel("Z12logOH")
+    plt.ylabel("[OIII]\u03BB5007 / [OII]\u03BB3727")
+    plt.autoscale(enable = True, axis = 'both', tight = None) # Creates a semilog plot to effectively show data
+    plt.semilogy(table["Z12logOH"][goodIndexes], fluxTable["OIII_5007_FLUX"][goodIndexes] / fluxTable["OII_3727_FLUX"][goodIndexes], ".")
     plt.show()
 
 
